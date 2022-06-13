3b7c2844087b9934d568015aaed9646526d235e5
Christian Glahn <cpglahn@gmail.com>
2022-06-13 17:29:16 +0200

    add search terms manually fixes #5

--- site/js/index.js
+++ site/js/index.js
@@ -3,12 +3,15 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
 
-toggleResultDetails(document.querySelector('.results'));
-dropSearchElement(document.querySelector('.searchoptions'));
-clearSearch(document.querySelector('#newsearch'));
-addSearchElement(document.querySelector(".sidebar"));
+toggleResultDetails();
+dropSearchElement();
 
-function toggleResultDetails(e) {
+clearSearch();
+addSearchElement();
+addSearchTerm();
+
+function toggleResultDetails() {
+    const e = document.querySelector('.results'); 
 
     e.addEventListener("click", function (evt) {
         const targetParent = evt.target.parentNode.parentNode;
@@ -30,9 +33,10 @@ function toggleResultDetails(e) {
     });
 } 
 
-function dropSearchElement(searchoptions) {
-    searchoptions.addEventListener("click", function (evt) {
+function dropSearchElement() {
+    const searchoptions = document.querySelector('.searchoptions');
 
+    searchoptions.addEventListener("click", function (evt) {
         var targetParent = evt.target.parentNode.parentNode;
 
         if (evt.target.classList.contains("optionclose"))  {
@@ -44,7 +48,105 @@ function dropSearchElement(searchoptions) {
     });
 } 
 
-function addSearchElement(sidebarelement) {
+function addSearchTerm() {
+    const searchFormElement = document.querySelector("#liveinput");
+    const searchFormButton = document.querySelector("#basic-addon2");
+    const searchTermElement = document.querySelector("#searchterms");
+
+    const template = document.querySelector('#searchoption');
+    const searchoptions = document.querySelector('#searchcontainer .searchoptions');
+
+    function handleSubmit(evt) {
+        const result = template.content.cloneNode(true);
+        const datafield = result.querySelector(".searchoption");
+
+        var currentValue = searchTermElement.value.trim();
+        searchTermElement.value = "";
+
+        var existingQuery = [...searchoptions.querySelectorAll(".searchoption")].filter(
+            element => element.innerText == currentValue && !element.classList.contains("bi-person-circle")
+        );
+
+        if (currentValue.startsWith("sdg:")) {
+            currentValue = currentValue.replace("sdg:", "").trim()
+
+            const sdg = parseInt(currentValue);
+
+            if (sdg >= 1 && sdg <= 16) {
+                datafield.classList.add("marker");
+
+                currentValue = `cat-${sdg < 10 ? "0" : ""}${sdg}`;
+
+                datafield.classList.add(currentValue);
+
+                existingQuery = [...searchoptions.querySelectorAll(".searchoption")].filter(element => 
+                    element.classList.contains(currentValue)
+                );
+            }
+            else {
+                // make existing query long.
+                existingQuery = [1];
+            }
+
+            currentValue = "";
+        }
+
+        if (currentValue.startsWith("dept:") || currentValue.startsWith("department:") ) {
+            currentValue = currentValue.toLowerCase().replace(/^dep(?:artmen)?t:/, "").trim().toUpperCase();
+
+            const depts = ["A", "G", "L", "N", "P", "S", "T", "W"];
+        
+            if (depts.includes(currentValue)) {
+                currentValue = `cat-${currentValue}`;
+
+                datafield.classList.add("marker");
+                datafield.classList.add(currentValue);
+
+                existingQuery = [...searchoptions.querySelectorAll(".searchoption")].filter(element => 
+                    element.classList.contains(currentValue)
+                );
+            }
+            else {
+                // make existing query long.
+                existingQuery = [1];
+            }
+
+            currentValue = "";
+        }
+
+        if (currentValue.startsWith("person:")) {
+            currentValue = currentValue.replace("person:", "").trim()
+            datafield.classList.add("bi-person-circle");
+            
+            existingQuery = [...searchoptions.querySelectorAll(".searchoption")].filter(element => 
+                element.innerText == currentValue && element.classList.contains("bi-person-circle")
+            );
+        }
+
+        datafield.innerText = currentValue;      
+        
+        
+        if (!existingQuery.length) {
+            searchoptions.appendChild(result);
+        }
+
+        evt.preventDefault();
+    }
+
+    searchFormElement.addEventListener("submit", handleSubmit);
+    searchFormButton.addEventListener("click", handleSubmit);
+}
+
+function liveQueryInput() {
+    const searchTermElement = document.querySelector("#searchterms");
+
+    searchTermElement.addEventListener("keyup", function(evt) {
+
+    });
+} 
+
+function addSearchElement() {
+    const sidebarelement = document.querySelector(".sidebar")
     const template = document.querySelector('#searchoption');
     const searchoptions = document.querySelector('#searchcontainer .searchoptions');
 
@@ -66,16 +168,14 @@ function addSearchElement(sidebarelement) {
     });
 }
 
-function clearSearch(searchoptions) {
-    searchoptions.addEventListener("click", function (evt) {
-        [...searchEntries].map(
-            opts => {
-                while (opts.firstChild) {
-                    opts.removeChild(opts.firstChild);
-                }
-            }
-        );
+function clearSearch() {
+    const searchoptions = document.querySelector('#newsearch');
+    const searchEntries = document.querySelector(".searchoptions");
 
+    searchoptions.addEventListener("click", function (evt) {
+        while (searchEntries.firstChild) {
+            searchEntries.removeChild(searchEntries.firstChild);
+        }
         // FIXME drop all entries from the search and display the default results
     });
 } 
