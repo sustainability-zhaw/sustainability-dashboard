9967e742613c86294668ee2dd6cd622d93b61d0d
Christian Glahn <cpglahn@gmail.com>
2023-11-20 19:43:28 +0100

    Clean remaining extras

--- site/js/models/SubTypes.mjs
+++ site/js/models/SubTypes.mjs
@@ -1,42 +1,12 @@
 import * as Logger from "../Logger.mjs";
-// import * as Logger from "../Logger.mjs";
 // import * as Filter from "./DqlFilter.mjs";
 import * as Events from "../Events.mjs";
-
-// import * as Filter from "./DqlFilter.mjs";
 
 Events.listen.startUserInterface(initUI);
 
 const Model = {
     types: []
 };
-
-async function initUI() {
-    Logger.debug("Subtype fetch: Initializing types");
-    await fetch("/api", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-            query: `{
-                queryInfoObjectSubType {
-                    name
-                }
-            }`
-        })
-    })
-        .then(async response => {
-            if (!response.ok) {
-                console.error("SubType fetch: Server responded with ", response.status);
-                return;
-            }
-            const result = await response.json();
-
-            Model.types = result.data.queryInfoObjectSubType.map(subType => subType.name);
-        })
-        .catch(error => {
-            console.error("SubType fetch:", error);
-        });
-}
 
 async function initUI() {
     Logger.debug("Subtype fetch: Initializing types");
