3731c633b875f17948bc6b81a56ff60ad2320346
Christian Glahn <cpglahn@gmail.com>
2023-11-20 19:47:42 +0100

    Use pure async/await instead of mixing await and promises

--- site/js/models/SubTypes.mjs
+++ site/js/models/SubTypes.mjs
@@ -10,7 +10,7 @@ const Model = {
 
 async function initUI() {
     Logger.debug("Subtype fetch: Initializing types");
-    await fetch("/api", {
+    const response = await fetch("/api", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
@@ -20,19 +20,21 @@ async function initUI() {
                 }
             }`
         })
-    })
-        .then(async response => {
-            if (!response.ok) {
-                console.error("SubType fetch: Server responded with ", response.status);
-                return;
-            }
-            const result = await response.json();
+    });
 
-            Model.types = result.data.queryInfoObjectSubType.map(subType => subType.name);
-        })
-        .catch(error => {
-            console.error("SubType fetch:", error);
-        });
+    if (!response.ok) {
+        console.error("SubType fetch: Server responded with ", response.status);
+        return;
+    }
+
+    try {
+        const result = await response.json();
+
+        Model.types = result.data.queryInfoObjectSubType.map(subType => subType.name);
+    }
+    catch (error) {
+        console.error("SubType fetch:", error);
+    }
 }
 
 export function getRecords() {
